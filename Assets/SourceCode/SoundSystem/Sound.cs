786d7502e72589f635451045decbb97c60677f5a
Maxens Thiam <MaxensThiam@outlook.com>
2023-09-07 16:13:38 +0200

    [ADD] SoundSystem

---
+++ Assets/SourceCode/SoundSystem/Sound.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+[Serializable]
+public class Sound
+{
+    public string name;
+    public AudioSource source;
+    public AudioClip clip;
+    [SerializeField, Range(0, 1)] public float volume = 0.5f;
+    [SerializeField,Range(-3,3)]public float pitch = 1;
+    public bool loop;
+    
+}
