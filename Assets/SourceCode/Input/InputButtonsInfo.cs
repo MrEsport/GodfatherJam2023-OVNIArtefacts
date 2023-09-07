4017cc9a7f7ea49869fb31108856609dc8792072
Maxens Thiam <MaxensThiam@outlook.com>
2023-09-07 09:01:12 +0200

    [ADD] InputButtonsScriptableObject

---
+++ Assets/SourceCode/Input/InputButtonsInfo.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputButtonsInfo : MonoBehaviour
+{
+    [CreateAssetMenu(fileName = "InputButtonsInfo", menuName = "ScriptableObjects/InputButtonsInfo", order = 3)]
+
+    List<InputButton> inputButtons;
+
+
+}
